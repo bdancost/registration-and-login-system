cc68ad8dc2c48a7d8c1fd9663770cc389f8eca53
Daniel Fernandes Costa <pr.danfc88@gmail.com>
2025-02-22 10:33:31 -0300

    Adicionando useStates no codigo

--- pages/cadastro.tsx
+++ pages/cadastro.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Link from "next/link";
 import styles from "../styles/Login.module.css";
 
@@ -6,13 +7,53 @@ import Input from "../src/components/input/input";
 import Button from "../src/components/button/button";
 
 export default function CadastroPage() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const handleFormEdit = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    name: string
+  ) => {
+    setFormData({
+      ...formData,
+      [name]: event.target.value,
+    });
+  };
+
   return (
     <div className={styles.background}>
       <LoginCard title="Crie sua conta">
         <form className={styles.form}>
-          <Input type="text" placeholder="Seu nome" />
-          <Input type="email" placeholder="Seu e-mail" />
-          <Input type="password" placeholder="Sua senha" />
+          <Input
+            type="text"
+            placeholder="Seu nome"
+            required
+            value={formData.name}
+            onChange={(e) => {
+              handleFormEdit(e, "name");
+            }}
+          />
+          <Input
+            type="email"
+            placeholder="Seu e-mail"
+            required
+            value={formData.email}
+            onChange={(e) => {
+              handleFormEdit(e, "email");
+            }}
+          />
+          <Input
+            type="password"
+            placeholder="Sua senha"
+            required
+            value={formData.password}
+            onChange={(e) => {
+              handleFormEdit(e, "password");
+            }}
+          />
           <Button>Cadastrar</Button>
           <Link href="/login">Ja possui uma conta? Entre</Link>
         </form>
